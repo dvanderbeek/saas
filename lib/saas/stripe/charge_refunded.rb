a383f5fba985677b5cf5785ab4ad19b04bea8e48
David Van Der Beek <earlynovrock@gmail.com>
2018-11-05 11:07:58 -0600

    track charge refunds

--- lib/saas/stripe/charge_refunded.rb
+++ lib/saas/stripe/charge_refunded.rb
@@ -2,9 +2,12 @@ module Saas
   module Stripe
     class ChargeRefunded
       def call(event)
-        puts "Charge refunded #{event}"
-        # TODO: Update amount refunded on charge model once it exists
-        # https://github.com/jasoncharnes/pay/commit/4be0d0591403ce6a599b4633d00248732da5a3a7
+        object = event.data.object
+        charge = ::Saas::Charge.find_by(stripe_id: object.id)
+        
+        return unless charge.present?
+        
+        charge.update(amount_refunded_cents: object.amount_refunded)
       end
     end
   end
