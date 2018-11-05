3bcff7cae042bc5bc0f2dc365b671c322adf32a9
David Van Der Beek <earlynovrock@gmail.com>
2018-11-05 11:22:33 -0600

    do not override status

--- app/models/saas/charge.rb
+++ app/models/saas/charge.rb
@@ -3,13 +3,25 @@ module Saas
     belongs_to :subscription
 
     def status
-      if amount_refunded_cents && amount_refunded_cents < amount_cents
+      if partial_refund?
         "Partial refund"
-      elsif amount_refunded_cents
+      elsif full_refund?
         "Full refund"
       else
         self[:status]
       end
+    end
+
+    def refunded?
+      amount_refunded_cents && amount_refunded_cents > 0
+    end
+
+    def partial_refund?
+      amount_refunded_cents && amount_refunded_cents > 0 && amount_refunded_cents < amount_cents
+    end
+
+    def full_refund?
+      amount_refunded_cents && amount_refunded_cents == amount_cents
     end
   end
 end
