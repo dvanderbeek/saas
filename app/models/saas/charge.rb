3be2abbb2191bac7de246443020c50761ae7cb51
David Van Der Beek <earlynovrock@gmail.com>
2018-11-05 11:18:12 -0600

    display refund status

--- app/models/saas/charge.rb
+++ app/models/saas/charge.rb
@@ -1,5 +1,15 @@
 module Saas
   class Charge < ApplicationRecord
     belongs_to :subscription
+
+    def status
+      if amount_refunded_cents && amount_refunded_cents < amount_cents
+        "Partial refund"
+      elsif amount_refunded_cents
+        "Full refund"
+      else
+        self[:status]
+      end
+    end
   end
 end
